2709db0c08244844767dd10c28433ae0ccbc6160
Renaat Debleu <rdebleu@eWallah.net>
2023-03-05 02:03:57 +0100

    code review

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'report_growth';
-$plugin->requires  = 2021051700;
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v4.0.2';
-$plugin->version   = 2023010601;
+$plugin->requires = 2021051700;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'v4.0.2';
+$plugin->version = 2023010601;
